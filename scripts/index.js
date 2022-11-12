cc045bf1c047f95b08c609e155f0d37329c78149
Sofia Piedra Cueva <spiedracueva@gmail.com>
2022-11-12 18:21:21 -0300

    Prueba 1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+/*
 let conditionNombre = false
 let conditionApellido = false
 const alertError = document.getElementById('alertError')
@@ -15,6 +16,7 @@ const results = document.getElementById('results')
 const inputPutNombre = document.getElementById('inputPutNombre')
 const inputPutApellido = document.getElementById('inputPutApellido')
 const urlMockapi = 'https://63651843f711cb49d1f4f824.mockapi.io/users'
+
 
 // llamamos al json con los users y lo imprimimos
 const fetchUsers = () => {
@@ -186,3 +188,130 @@ document.addEventListener("mouseup", function (event) {
         alertError.classList.add("d-none");
     }
 });
+*/
+
+
+// URL de la BASE DE DATOS y 
+const URL_Mockapi = 'https://63651843f711cb49d1f4f824.mockapi.io/users'
+
+// GET - Empieza
+// Llamamos al campo (id) y el botón, y donde se van a imprimir los datos
+
+const results = document.getElementById("results");
+const inputGet1Id = document.getElementById("inputGet1Id");
+const btnGet1 = document.getElementById("btnGet1");
+
+// Función para mostrar los datos a recibir en pantalla
+
+function showData(users) {
+
+    let HtmlResultsToAdd = ''
+
+    if (!inputGet1Id.value) {          
+        for (let user of users) {  
+            HtmlResultsToAdd += `   
+            <li>
+                <p>ID: ${user.id}</p>
+                <p>NAME: ${user.name}</p>
+                <p>LASTNAME: ${user.lastname}</p>
+            </li>
+            <br>`  // Se podría hacer una sola función para el HTMLtoAppend
+            
+        }
+    } else {
+        let user = users.filter(userId => userId.id == inputGet1Id.value)[0]
+        HtmlResultsToAdd += `
+        <li>
+            <p>ID: ${user.id}</p>
+            <p>NAME: ${user.name}</p>
+            <p>LASTNAME: ${user.lastname}</p>
+        </li>
+        <br>`
+
+    }
+    results.innerHTML = HtmlResultsToAdd
+};
+
+// Hacemos el FETCH con el método GET
+
+const fetchGET = () => {
+    fetch(URL_Mockapi)
+    .then(response => {
+        if (response.ok) {
+          return response.json();
+        }else{
+          throw Error(response.statusText); // Esto se puede usar después para la alerta
+        }
+    })
+    .then((users) => showData(users))
+    .catch((error) => {
+        console.log(error);
+        //alertError.classList.remove('d-none') -- Ver después de que forma hacer la alerta
+    });      
+};
+
+// Agregar funcionalidad al botón del GET
+
+btnGet1.addEventListener('click', () => {
+    fetchGET();
+})
+
+// GET - Termina
+
+// POST - Empieza
+// Llamamos a los dos campos (nombre y apellido), y el botón
+
+const inputPostNombre = document.getElementById("inputPostNombre");
+const inputPostApellido = document.getElementById("inputPostApellido");
+const btnPost = document.getElementById("btnPost");
+
+// Modelo de nuevo usuario
+
+const makeUser = () => (
+    {
+    name: inputPostNombre.value,
+    lastname: inputPostApellido.value
+    }
+);
+
+// Hacemos el FETCH con el método POST
+
+const fetchPOST = () => {
+    fetch(URL_Mockapi, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"}, // Es necesario este header aca ?
+        body: JSON.stringify(makeUser()),
+    })
+    .then(response => {
+        if (response.ok) {
+          return response.json();
+        }else{
+          throw Error(response.statusText); // Esto se puede usar después para la alerta
+        }
+    })
+    .then((users) => fetchGET(users))
+    .catch((error) => {
+        console.log(error);
+        //alertError.classList.remove('d-none') -- Ver después de que forma hacer la alerta
+    });
+};
+
+// Habilitar el botón POST
+
+const btnPOSTCheck = () => {
+    if (inputPostNombre.value !== '' && inputPostApellido.value !== '') {
+        btnPost.disabled = false
+    } else {
+        btnPost.disabled = true
+    }
+};
+
+// Agregar funcionalidad al botón del POST
+
+btnPost.addEventListener('click', () => {
+    fetchPOST();
+})
+
+// POST - Termina
+
+// PUT - Empieza
